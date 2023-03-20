7ee5aa9465520a4cfe82e961dd5ec1bec98310dd
rosbel <rosbelcr@gmail.com>
2023-03-20 18:02:41 -0400

    fetching data Get ListPokemons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next"
+import { NextPage, GetStaticProps } from "next"
+import { pokeApi } from "../api";
 import { Layout } from "../components/layouts/layout";
+import { PokemonListResponse } from "../interfaces";
 
- const Home: NextPage = () => {
+ const Home: NextPage = (props) => {
+    console.log(props)
+
     return (
         <Layout title='Listado de pokemons'>
             
-            <Button color="gradient">Hola Mundo</Button>
+            <ul>
+                <li>Pokemon</li>
+                <li>Pokemon</li>
+                <li>Pokemon</li>
+                <li>Pokemon</li>
+                <li>Pokemon</li>
+            </ul>
 
         </Layout>
     )
 }
+
+
+export const getStaticProps: GetStaticProps = async (props) => {
+    // const { data } = await  // your fetch function here 
+    const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+    //console.log({data});
+
+    console.log(props)
+    return {
+        props: {
+            pokemons: data.results
+        }
+    }
+}
+
 
 export default Home;
