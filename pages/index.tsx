c5659b6adfa9cc8589767638bd5d17a4cc875e58
rosbel <rosbelcr@gmail.com>
2023-03-21 02:33:06 -0400

    Main Grid done!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,29 @@
+import { Grid } from "@nextui-org/react";
 import { NextPage, GetStaticProps } from "next"
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts/layout";
-import { PokemonListResponse } from "../interfaces";
+import { PokemonCard } from "../components/pokemon/PokemonCard";
+import { PokemonListResponse, SmallPokemon } from "../interfaces";
 
- const Home: NextPage = (props) => {
-    console.log(props)
+
+
+interface Props  {
+    pokemons: SmallPokemon[]
+}
+
+ const Home: NextPage<Props> = (props) => {
+    //console.log(props)
 
     return (
         <Layout title='Listado de pokemons'>
             
-            <ul>
-                <li>Pokemon</li>
-                <li>Pokemon</li>
-                <li>Pokemon</li>
-                <li>Pokemon</li>
-                <li>Pokemon</li>
-            </ul>
+            <Grid.Container gap={2} justify='flex-start'>
+                {
+                props.pokemons.map((pokemon) =>(
+                    <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+                )) 
+                }
+            </Grid.Container>
 
         </Layout>
     )
@@ -25,12 +33,21 @@ import { PokemonListResponse } from "../interfaces";
 export const getStaticProps: GetStaticProps = async (props) => {
     // const { data } = await  // your fetch function here 
     const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
-    //console.log({data});
+    //console.log(data);
 
-    console.log(props)
+    //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/130.svg
+    
+    const pokemons : SmallPokemon[] = data.results.map((poke , i)=>({
+        ...poke,
+        id: i + 1,
+        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+            
+        }
+    ))
+
     return {
         props: {
-            pokemons: data.results
+            pokemons
         }
     }
 }
